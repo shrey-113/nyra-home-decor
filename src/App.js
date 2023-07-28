387dd1ff5c0b14549e7a1ffb9b3afdb66c019d67
Shrey-113 <shreyanshngp11@gmail.com>
2023-07-29 00:43:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import IndexScreen from "./screens/indexScreen";
+
+function App() {
+  return (
+    <div className="App">
+      <IndexScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/indexScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function IndexScreen() {
+  return (
+    <>
+      <div className="main--card">
+        <h1>Nyra Home Decor</h1>
+      </div>
+    </>
+  );
+}
