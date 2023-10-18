2a182c82b577b370c80ecc5a9f17d84b92f88c39
shrey-113 <shreyanshngp11@gmail.com>
2023-10-18 22:59:17 +0530

    Added server

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "./componentscss.css";
 import "../App.css";
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <header> 
+    <header>
       <div className="nyra-home-decor-original">
         <span className="nyra-home-decor-red">N</span>
         <span className="nyra-home-decor-violet">YRA</span> &nbsp; HOME DECOR
       </div>
       <ul className="nav-items">
-        <li className="selected">HOME</li>
-        <li>OWN DESIGN?</li>
-        <li>CONTACT US</li>
+        <li>
+          <Link to="/" className="header--link selected">
+            HOME
+          </Link>
+        </li>
+        <li>
+          <Link to="/own">OWN DESIGN?</Link>
+        </li>
+        <li>
+          <Link to="/contact">CONTACT US</Link>
+        </li>
       </ul>
     </header>
   );
