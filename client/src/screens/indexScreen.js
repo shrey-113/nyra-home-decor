2f10530168eccb672310958a0bc7653404bb5be7
unknown <jainadi1717@gmail.com>
2023-10-26 01:12:04 +0530

    better slider

--- client/src/screens/indexScreen.js
+++ client/src/screens/indexScreen.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "../components/Header";
 import background from "../assets/main-bg.png";
 import image from "../assets/img1.png";
 import { Link } from "react-router-dom";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import ReactDOM from 'react-dom';
 
 export default function IndexScreen() { 
   return (
@@ -47,17 +50,35 @@ export default function IndexScreen() {
           </div>
           <div className="rightcolumn">
           <div className="indexscreen-image-new">
-              <img src={image} alt="Your Image" className="full-size-image" />
+          <Carousel className="carousel-main"
+          autoPlay
+          infiniteLoop
+          swipeable
+          useKeyboardArrows
+          >
+                <div>
+                    <img src="https://images.unsplash.com/photo-1487088678257-3a541e6e3922?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    {/* <p className="legend">Legend 1</p> */}
+                </div>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1487088678257-3a541e6e3922?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    {/* <p className="legend">Legend 2</p> */}
+                </div>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?auto=format&fit=crop&q=80&w=2069&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    {/* <p className="legend">Legend 3</p> */}
+                </div>
+            </Carousel>
             </div>
           </div>
         </div>
-        {/* <div className="button-container">
+        <div className="button-container">
           <button className="explore-button">
             <span className="explore-text">Explore</span>
             <div className="explore-line"></div>
             <span className="explore-icon">&#xf178;</span>
           </button>
-        </div> */}
+        </div>
       </div>
     </>
   );
